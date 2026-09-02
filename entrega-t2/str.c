b83d4f026dbd4d88ed133a7906d0dd71b3121990
Tiago <tiago.zemolin@gmail.com>
2026-09-01 21:47:23 -0300

    operações de acesso, busca e comparação

--- entrega-t2/str.c
+++ entrega-t2/str.c
@@ -138,13 +138,70 @@ Str s_cria_de_arquivo(char *nome)
         return s;
     }
 
-    if (fseek ((arquivo, 0, SEEK_END) != 0))
+    if (fseek (arquivo, 0, SEEK_END) != 0)
     {
         fclose (arquivo);
         return s;
     }
-    
+    long tam = ftell(arquivo);
+    if (tam < 0) 
+    {
+        fclose (arquivo);
+        return s;
+    }
+    if (fseek (arquivo, 0, SEEK_SET) != 0)
+    {
+        fclose (arquivo);
+        return s;
+    }
+    if (tam == 0)
+    {
+        fclose (arquivo);
+        return s;
+    }
 
+    s->nbytes = tam;
+
+    s->cap = MIN_ALLOC;
+    while (s->cap < s->nbytes) 
+    {
+        s->cap *= 2;
+    }
+    s->dados = malloc (s->cap);
+    if (s->dados == NULL) {
+        fclose (arquivo);
+
+        s->n = 0;
+        s->nbytes = 0;
+        s->cap = 0;
+
+        return s;
+    }
+
+    size_t lidos = fread (s->dados, 1, s->nbytes, arquivo);
+    fclose (arquivo);
+    if (lidos != s->nbytes) 
+    {
+        free (s->dados);
+
+        s->dados = NULL;
+        s->n = 0;
+        s->nbytes = 0;
+        s->cap = 0;
+
+        return s;
+    }
+    s->n = u8_conta_unichar_nos_bytes (s->nbytes, s->dados);
+
+    if (s->n < 0) {
+        free (s->dados);
+        s->dados = NULL;
+        s->n = 0;
+        s->nbytes = 0;
+        s->cap = 0;
+
+        return s;
+    }
     return s;
 }
 
@@ -153,22 +210,35 @@ Str s_cria_de_arquivo(char *nome)
 int s_tam(Str_c s)
 {
     s_ok(s);
-    //...
-    return 0;
+    return s->n;
 }
 
 char *s_strc(Str_c s)
 {
     s_ok(s);
-    //...
-    return NULL;
+    char *str = malloc(s->nbytes + 1);
+    assert (str != NULL);
+
+    memcpy (str, s->dados, s->nbytes);
+    str[s->nbytes] = '/0';
+
+    return str;
 }
 
 unichar s_ch(Str_c s, int pos)
 {
     s_ok(s);
-    //...
-    return UNI_INV;
+    if (pos < 0 || pos >= s->n) 
+    {
+        return UNI_INV;
+    }
+
+    byte *ptr = u8_avanca_unichar (s->dados, pos);
+
+    unichar c;
+    u8_unichar_nos_bytes (4, ptr, &c);
+
+    return c;
 }
 
 // operações de busca e comparação {{{1
@@ -177,15 +247,44 @@ bool s_igual(Str_c s, Str_c sb)
 {
     s_ok(s);
     s_ok(sb);
-    //...
-    return false;
+    if (s->n != sb->n) 
+    {
+        return false;
+    }
+    
+    if (s->nbytes != sb->nbytes)
+    {
+        return false;
+    }
+
+    return memcmp(s->dados, sb->dados, s->nbytes) == 0;
 }
 
 int s_busca_c(Str_c s, int pos, Str_c sb)
 {
     s_ok(s);
     s_ok(sb);
-    //...
+    if (pos < 0) 
+    {
+        pos = 0;
+    }
+    if (pos >= s->n || sb -> n == 0)
+    {
+        return -1;
+    }
+    for (int i = pos; i < s-> n; i++) 
+    {
+        unichar c = s_ch(s, i);
+
+        for (int j = 0; i < sb -> n; j++)
+        {
+            if (c == s_ch(sb, j)) 
+            {
+                return i;
+            }
+        }
+    }
+
     return -1;
 }
 
@@ -193,7 +292,31 @@ int s_busca_nc(Str_c s, int pos, Str_c sb)
 {
     s_ok(s);
     s_ok(sb);
-    //...
+
+    if (pos < 0)
+        pos = 0;
+
+    if (pos >= s->n)
+        return -1;
+
+    for (int i = pos; i < s->n; i++)
+    {
+        unichar c = s_ch(s, i);
+        bool pertence = false;
+
+        for (int j = 0; j < sb->n; j++)
+        {
+            if (c == s_ch(sb, j))
+            {
+                pertence = true;
+                break;
+            }
+        }
+
+        if (!pertence)
+            return i;
+    }
+
     return -1;
 }
 
@@ -201,7 +324,27 @@ int s_busca_rc(Str_c s, int pos, Str_c sb)
 {
     s_ok(s);
     s_ok(sb);
-    //...
+    if (pos < 0) 
+    {
+        pos = 0;
+    }
+    if (pos >= s->n || sb -> n == 0)
+    {
+        return -1;
+    }
+    for (int i = pos - 1; i >= 0; i--) 
+    {
+        unichar c = s_ch(s, i);
+
+        for (int j = 0; i < sb -> n; j++)
+        {
+            if (c == s_ch(sb, j)) 
+            {
+                return i;
+            }
+        }
+    }
+
     return -1;
 }
 
@@ -209,7 +352,31 @@ int s_busca_rnc(Str_c s, int pos, Str_c sb)
 {
     s_ok(s);
     s_ok(sb);
-    //...
+
+    if (pos > s->n)
+        pos = s->n;
+
+    if (pos <= 0)
+        return -1;
+
+    for (int i = pos - 1; i >= 0; i--)
+    {
+        unichar c = s_ch(s, i);
+        bool pertence = false;
+
+        for (int j = 0; j < sb->n; j++)
+        {
+            if (c == s_ch(sb, j))
+            {
+                pertence = true;
+                break;
+            }
+        }
+
+        if (!pertence)
+            return i;
+    }
+
     return -1;
 }
 
@@ -217,7 +384,33 @@ int s_busca_s(Str_c s, int pos, Str_c buscada)
 {
     s_ok(s);
     s_ok(buscada);
-    //...
+
+    if (pos < 0)
+        pos = 0;
+
+    if (pos > s->n)
+        pos = s->n;
+
+    if (buscada->n == 0)
+        return pos;
+
+    for (int i = pos; i <= s->n - buscada->n; i++)
+    {
+        bool igual = true;
+
+        for (int j = 0; j < buscada->n; j++)
+        {
+            if (s_ch(s, i + j) != s_ch(buscada, j))
+            {
+                igual = false;
+                break;
+            }
+        }
+
+        if (igual)
+            return i;
+    }
+
     return -1;
 }
 
